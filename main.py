19f17a8467443989f1162aff585dcc8667ce5156
CreamBBQ <jgmd1306@gmail.com>
2022-02-21 11:26:19 -0300

    Change file reader to pandas becaouse i have problems with google cloud and external files

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 import tweepy
 import credentials
 import random
-import csv
+import pandas as pd
 
 
-def get_tweet_string(): 
-    with open("korean_movies.csv", "r", encoding='utf-8') as file:
-        csv_reader = csv.reader(file)
-        rows = list(csv_reader)
-        random_integer = random.randint(1,923)
-        random_row = rows[random_integer]
-    name = random_row[0]
-    year = random_row[1]
-    stars = random_row[2]
-    abstract = random_row[3]
+def get_tweet_string():
+    movies_info = pd.read_csv(
+        'https://raw.githubusercontent.com/CreamBBQ/webScraping_IDMb/master/korean_movies.csv')
+    random_integer = random.randint(0, 922)
+    random_movie = movies_info.iloc[[random_integer]]
+    name = random_movie.iloc[0, 0]
+    year = random_movie.iloc[0, 1]
+    stars = random_movie.iloc[0, 2]
+    abstract = random_movie.iloc[0, 3]
     string = f"{name} {year}\n⭐{stars}\n\n{abstract}"
     return string, random_integer
-    
-    
+
+
 def get_client():
     client = tweepy.Client(
-    bearer_token=credentials.bearer,
-    consumer_key=credentials.key,
-    consumer_secret=credentials.key_secret,
-    access_token=credentials.token,
-    access_token_secret=credentials.token_secret)
+        bearer_token=credentials.bearer,
+        consumer_key=credentials.key,
+        consumer_secret=credentials.key_secret,
+        access_token=credentials.token,
+        access_token_secret=credentials.token_secret)
     return client
 
 
@@ -32,8 +31,8 @@ def tweet():
     client = get_client()
     movie_info, random_integer = get_tweet_string()
     if len(movie_info) < 281:
-        client.create_tweet(text = movie_info)
-    else: 
+        client.create_tweet(text=movie_info)
+    else:
         except_msg = f"@CreamBBQ La película ubicada en la fila {random_integer} superó el límite de carácteres"
         client.create_tweet(text=except_msg)
 
@@ -44,9 +43,3 @@ def run():
 
 if __name__ == '__main__':
     run()
-
-
-
-
-
-
