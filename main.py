db2841f5ec4f9525b647fb9975b3d4dc83924296
CreamBBQ <jgmd1306@gmail.com>
2022-02-21 09:11:06 -0300

    Add tweet function with the movie's information. In google cloud im gonna deploy the script

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import tweepy
 import credentials
+import random
+import csv
 
 
+def get_tweet_string(): 
+    with open("korean_movies.csv", "r", encoding='utf-8') as file:
+        csv_reader = csv.reader(file)
+        rows = list(csv_reader)
+        random_integer = random.randint(1,923)
+        random_row = rows[random_integer]
+    name = random_row[0]
+    year = random_row[1]
+    stars = random_row[2]
+    abstract = random_row[3]
+    string = f"{name} {year}\n⭐{stars}\n\n{abstract}"
+    return string, random_integer
+    
+    
 def get_client():
     client = tweepy.Client(
     bearer_token=credentials.bearer,
@@ -12,15 +28,25 @@ def get_client():
     return client
 
 
-def get_user_info():
+def tweet():
     client = get_client()
-    user = client.get_user(username="CreamBBQ")
-    return user.data.name
+    movie_info, random_integer = get_tweet_string()
+    if len(movie_info) < 281:
+        client.create_tweet(text = movie_info)
+    else: 
+        except_msg = f"@CreamBBQ La película ubicada en la fila {random_integer} superó el límite de carácteres"
+        client.create_tweet(text=except_msg)
+
+
+def run():
+    tweet()
+
+
+if __name__ == '__main__':
+    run()
 
 
 
-client = get_client()
-client.create_tweet(text = 'Hola mundo')
 
 
 
